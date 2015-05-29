f2b47b0eda107d569e4341e4aa91a9c75bd29993
deryagin <deryagin.and@gmail.com>
2015-05-29 22:52:27 +0300

    New. Добавлен chdir и глобальный CWD. Глобавльный CWD -- не гуд.

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ process.chdir(__dirname);
 global.CWD = process.cwd() + '/';
 
 /**
- * process.cwd() вынести в global.CWD.
+ * process.cwd() вынести в global.CWD. Хотя, конечно, CWD лучше
+ * в глобал не пихать. А то вдруг какая-либо либа его затрет.
  * Попробывать поиграться с provess.paths и require.resolve()
  * Попробывать убрать node_modules с глаз долой из корня проекта
  * напр. в lib/node_modules.
  */
-var config = require(process.cwd() + '/config');
-var Application = require(process.cwd() + '/src/Application');
+var config = require(CWD + 'config');
+var Application = require(CWD + 'src/Application');
 
 var application = new Application(config);
 application.start();
