852d686ef7d343973092f3915a064bdf3b98cd9e
deryagin <deryagin.and@gmail.com>
2015-06-29 19:03:00 +0300

    New. Доработка алгоритма работы с ограничивающей очередью.

--- src/LimitedQueue.js
+++ src/LimitedQueue.js
@@ -16,11 +16,11 @@ function S3Uploader_LimitedQueue(emitter, config) {
   /** @type {tasks-queue.TaskQueue} */
   var _taskQueue = new TaskQueue();
 
+  /** @type {{localPath: 'String', fsStats: 'fs.Stats'}} - контекст текущего, обрабатываемого события. */
+  var _eventContext = null;
+
   /** @type {Jinn} - вызов jinn.done() завершает обработку текущего события https://github.com/tutukin/tasks-queue */
   var _jinn = null;
-
-  /** @type {{localPath: 'String', fsStats: 'fs.Stats'}} - контекст текущего, обрабатываемого события. */
-  var _context = null;
 
   (function _initialize() {
     _taskQueue.setMinTime(config.defaultInterval);
@@ -37,11 +37,7 @@ function S3Uploader_LimitedQueue(emitter, config) {
    * @listens {S3Uploader_EventType.EMERGED_FILE}
    */
   self.addFileToQueue = function addFileToQueue(localPath, fsStats) {
-    _context = {
-      'localPath': localPath,
-      'fsStats': fsStats
-    };
-    queue.pushTask(FILE_ADDED_EVENT, _context)
+    _taskQueue.pushTask(FILE_ADDED_EVENT, { 'localPath': localPath, 'fsStats': fsStats })
   };
 
   /**
@@ -50,7 +46,7 @@ function S3Uploader_LimitedQueue(emitter, config) {
    */
   self.speedUpProcessing = function speedUpProcessing() {
     // если все ок, то сбрасываем интервал ожидания обработки следующего события и
-    // завершает его обработку вызовом _jinn.done()
+    // завершаем обработку текущего события вызовом _jinn.done()
     _taskQueue.setMinTime(config.defaultInterval);
     _jinn.done();
     _jinn = null;
@@ -61,19 +57,23 @@ function S3Uploader_LimitedQueue(emitter, config) {
    * @listens {S3Uploader_EventType.MOVE_FAILING}
    */
   self.slowDownProcessing = function slowDownProcessing() {
-    // если ошибка, добавляем таск в конец очереди,
-    // чтобы снова попытаться отправить файл в S3
-    // и увеличиваем интервал между попытками
+    // если ошибка, добавляем таск в конец очереди, чтобы снова попытаться отправить файл в S3
+    // увеличиваем интервал между попытками, завершаем обработку текущего события вызовом _jinn.done()
     slowDownTaskQueue();
-    _taskQueue.pushTask('file:added', _context);
+    _taskQueue.pushTask(FILE_ADDED_EVENT, _eventContext);
     _jinn.done();
     _jinn = null;
   };
 
-  function raiseMoveNeededEvent(jinn, context) {
+  /**
+   * @param jinn
+   * @param eventContext
+   * @listens {FILE_ADDED_EVENT}
+   */
+  function raiseMoveNeededEvent(jinn, eventContext) {
     _jinn = jinn;
-    _context = context;
-    emitter.emitMoveNeededEvent(context.localPath, context.fsStats);
+    _eventContext = eventContext;
+    emitter.emitMoveNeededEvent(eventContext.localPath, eventContext.fsStats);
   }
 
   function slowDownTaskQueue() {
