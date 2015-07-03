5c983c9372012e6c29640f6a13931d9dadb08dfa
deryagin <deryagin.and@gmail.com>
2015-07-03 19:18:51 +0300

    New. Добавлены заглушки для тестирования логгера.

---
+++ test/LoggerTest.js
@@ -0,0 +1,20 @@
+var logger = s3uploader.require('S3Uploader_Logger');
+
+describe('S3Uploader_LoggerTest', function () {
+
+  it('logSuccess: contains "SUCCESS" substring.', function () {
+
+  });
+
+  it('logSuccess: contains ISO8601 date substring.', function () {
+
+  });
+
+  it('logError: contains "ERROR" substring.', function () {
+
+  });
+
+  it('logError: contains ISO8601 date substring.', function () {
+
+  });
+});
