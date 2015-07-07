e43f8026c5e576c80d84d0eb4b3263a490b7e3f5
deryagin <deryagin.and@gmail.com>
2015-07-07 20:14:00 +0300

    New. Добавлен тест для Logger, перехватывающий и проверяющий stdout/stderr.

--- test/LoggerTest.js
+++ test/LoggerTest.js
@@ -1,20 +1,53 @@
-var logger = s3uploader.require('S3Uploader_Logger');
+var assert = require('chai').assert;
+var StreamFixture = require('fixture-stdout');
+var Logger = s3uploader.require('S3Uploader_Logger');
 
 describe('S3Uploader_LoggerTest', function () {
 
-  it('logSuccess: contains "SUCCESS" substring.', function () {
+  /** @type {S3Uploader_Logger} */
+  var _logger = new Logger();
 
+  /** @type {String} - данные, выводимые в stdout/stderr в тестах */
+  var _output = '';
+
+  /** @type {RegExp} - ISO8601 */
+  var ISO8601 = /.*\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d{3}Z.*/;
+
+  beforeEach(function () {
+    _output = '';
   });
 
-  it('logSuccess: contains ISO8601 date substring.', function () {
+  it('logSuccess() outputs ISO8601 date, "SUCCESS" substring, and "from", "to" parameters', function () {
+    var fixture = new StreamFixture(process.stdout);
 
+    fixture.capture(onWrite);
+    _logger.logSuccess('fromURI', 'toURI');
+    fixture.release();
+
+    assert.match(_output, ISO8601);
+    assert.include(_output, 'SUCCESS');
+    assert.include(_output, 'fromURI');
+    assert.include(_output, 'toURI');
   });
 
-  it('logError: contains "ERROR" substring.', function () {
+  it('logError() outputs ISO8601 date, "ERROR" substring, "from", "to" parameters, and the error message', function () {
+    var fixture = new StreamFixture(process.stderr);
+    var message = 'any error message';
+    var error = new Error(message);
 
+    fixture.capture(onWrite);
+    _logger.logError(error, 'fromURI', 'toURI');
+    fixture.release();
+
+    assert.match(_output, ISO8601);
+    assert.include(_output, 'ERROR');
+    assert.include(_output, 'fromURI');
+    assert.include(_output, 'toURI');
+    assert.include(_output, message);
   });
 
-  it('logError: contains ISO8601 date substring.', function () {
-
-  });
+  function onWrite(string) {
+    _output += string;
+    return false;
+  }
 });
