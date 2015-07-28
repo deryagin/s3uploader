dd6fc404f3e9c87cd0020128588a0faaedcfad0f
deryagin <deryagin.and@gmail.com>
2015-07-28 21:45:57 +0300

    New. Добавлен тест для конфигурации.

---
+++ test/ConfigurationTest.js
@@ -0,0 +1,31 @@
+var Configuration = s3uploader.require('S3Uploader_Configuration');
+var assert = require('chai').assert;
+
+describe('S3Uploader_Configuration', function () {
+
+  /** @type {S3Uploader_Configuration} */
+  var _config = null;
+
+  beforeEach(function () {
+    _config  = new Configuration();
+  });
+
+  it('Instance S3Uploader_Configuration has valid chokidar configuration', function () {
+    assert.propertyVal(_config.chokidar, 'path', '/var/spool/mcn/vpbx/records');
+    assert.property(_config.chokidar.options, 'alwaysStat');
+    assert.property(_config.chokidar.options, 'depth');
+  });
+
+  it('Instance S3Uploader_Configuration has valid tasks_queue configuration', function () {
+    assert.property(_config.tasks_queue, 'defaultInterval');
+    assert.property(_config.tasks_queue, 'maximumInterval');
+    assert.property(_config.tasks_queue, 'intervalMultiplier');
+  });
+
+  it('Instance S3Uploader_Configuration has valid knox configuration', function () {
+    assert.property(_config.knox, 'key');
+    assert.property(_config.knox, 'secret');
+    assert.property(_config.knox, 'bucket');
+    assert.propertyVal(_config.knox, 'style', 'path');
+  });
+});
